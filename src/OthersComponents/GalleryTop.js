2492f57f8c2a27fc12e12f939479d719f0e1c01b
sekul hassan <sekulhassan3031@gmail.com>
2023-12-20 21:08:09 +0600

    About and Gallery Done

---
+++ src/OthersComponents/GalleryTop.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Col, Container, Row} from "react-bootstrap";
+
+function GalleryTop(props) {
+    return (
+        <Container fluid="true" className="galleryTop mb-4">
+            <Row className="mx-0 px-0 overLayGalleryTop">
+                <Col className="mx-0 px-0">
+                    <div className="text-center overlayContentGalleryTop">
+                        <h4 className="title text-light">Hall of frame</h4>
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default GalleryTop;
