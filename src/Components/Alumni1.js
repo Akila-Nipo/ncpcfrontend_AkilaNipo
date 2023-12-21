4aca57ec051d87810e2664277c8b50bee58de878
Akila-Nipo <104201045+Akila-Nipo@users.noreply.github.com>
2023-12-21 13:11:01 +0600

    counter_modified

--- src/Components/Alumni1.js
+++ src/Components/Alumni1.js
@@ -27,7 +27,7 @@ function Alumni1(props) {
                                     <img className="carouselImgAlumni" src={fifteen} alt=""/>
                                     <div className="carouselCaptionAlumni">
                                         <h3 className="whiteTitle">JU_Assassins</h3>
-                                        <p className="alumniDescription text-white">Aninda Majumder(CSE), Md. Nafis Sadique(IIT), Suman Bhadra(CSE) participated at the ACM-ICPC 2015 World Finals and solved 6 problems.</p>
+                                        <p className="alumniDescription text-white text-center">Aninda Majumder(CSE), Md. Nafis Sadique(IIT), Suman Bhadra(CSE) participated at the ACM-ICPC 2015 World Finals and solved 6 problems.</p>
                                     </div>
                                 </div>
                             </Carousel.Item>
@@ -36,7 +36,7 @@ function Alumni1(props) {
                                     <img className="carouselImgAlumni" src={img6} alt=""/>
                                     <div className="carouselCaptionAlumni">
                                         <h3 className="whiteTitle">JU_O(N^3) </h3>
-                                        <p className="alumniDescription text-white">Md. Nafis Sadique(IIT), Niloy Datta(IIT), Raihat Zaman Neloy(CSE) secured 50th place at the ACM-ICPC 2016 World Finals solving 6 problems. </p>
+                                        <p className="alumniDescription text-white text-center">Md. Nafis Sadique(IIT), Niloy Datta(IIT), Raihat Zaman Neloy(CSE) secured 50th place at the ACM-ICPC 2016 World Finals solving 6 problems. </p>
                                     </div>
                                 </div>
                             </Carousel.Item>
@@ -45,7 +45,7 @@ function Alumni1(props) {
                                     <img className="carouselImgAlumni" src={_2017} alt=""/>
                                     <div className="carouselCaptionAlumni">
                                         <h3 className="whiteTitle">JU_Circavex </h3>
-                                        <p className="alumniDescription text-white">Bir Bahadur Khatri, Md Sahedul Islam Sohel, Raihat Zaman Neloy at the ACM-ICPC 2017 World Finals. </p>
+                                        <p className="alumniDescription text-white text-center">Bir Bahadur Khatri, Md Sahedul Islam Sohel, Raihat Zaman Neloy at the ACM-ICPC 2017 World Finals. </p>
                                     </div>
                                 </div>
                             </Carousel.Item>
@@ -54,7 +54,7 @@ function Alumni1(props) {
                                     <img className="carouselImgAlumni" src={_2021} alt=""/>
                                     <div className="carouselCaptionAlumni">
                                         <h3 className="whiteTitle">JU_Glass_Half_Full</h3>
-                                        <p className="alumniDescription text-white">Anik Sarker, Chayan Kumar Ray, Sakib Hasan at the ICPC 2021 World Finals Dhaka.</p>
+                                        <p className="alumniDescription text-white text-center">Anik Sarker, Chayan Kumar Ray, Sakib Hasan at the ICPC 2021 World Finals Dhaka.</p>
                                     </div>
                                 </div>
                             </Carousel.Item>
@@ -63,7 +63,7 @@ function Alumni1(props) {
                                     <img className="carouselImgAlumni" src={img1} alt=""/>
                                     <div className="carouselCaptionAlumni">
                                     <h3 className="whiteTitle">JU_kzvd4729</h3>
-                                        <p className="alumniDescription text-white">JU_kzvd4729 led by Mahmudul Hasan Shanto(CSE-46), Richita Khandakar Rifat(CSE-46) and Alif Al Hasan(CSE-47) secured 3rd place at the 2021 ICPC Asia Regional Dhaka Contest.</p>
+                                        <p className="alumniDescription text-white text-center">JU_kzvd4729 led by Mahmudul Hasan Shanto(CSE-46), Richita Khandakar Rifat(CSE-46) and Alif Al Hasan(CSE-47) secured 3rd place at the 2021 ICPC Asia Regional Dhaka Contest.</p>
                                     </div>
                                 </div>
                             </Carousel.Item>
